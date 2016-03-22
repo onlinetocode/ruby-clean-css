b64ac703b3eb9b54d9a2638bc9e435f7cd82b897
Maurice Kock <kock.maurice@googlemail.com>
2016-03-22 23:05:02 +0100

    removed if statement

--- lib/ruby-clean-css/sprockets.rb
+++ lib/ruby-clean-css/sprockets.rb
@@ -4,19 +4,7 @@ module RubyCleanCSS::Sprockets
 
   def self.register(sprockets)
     klass = RubyCleanCSS::Compressor
-    if sprockets.respond_to?(:register_compressor)
-      sprockets.register_compressor('text/css', LABEL, klass.new)
-      sprockets.css_compressor = LABEL
-    elsif sprockets.respond_to?(:register_preprocessor)
-      sprockets.register_preprocessor('text/css', LABEL, klass.new)
-      sprockets.css_compressor = LABEL
-    else
-      Sprockets::Compressors.register_css_compressor(
-        LABEL,
-        klass.to_s,
-        :default => true
-      )
-    end
+    Sprockets.register_compressor 'text/css', LABEL, klass
   end
 
 end
